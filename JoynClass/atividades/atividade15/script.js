42b834bd19b252f706666bd29e297cc5ee1cf310
Samuel Santana <samuelsantana95@outlook.com>
2023-07-29 17:51:17 -0300

    Update script.js

--- JoynClass/atividades/atividade15/script.js
+++ JoynClass/atividades/atividade15/script.js
@@ -2,30 +2,26 @@
  A partir dessas informações, o sistema mostrará o nome do usuário e a idade que completou, ou completará, no ano atual (2022).
 Caso o usuário não digite um número ou apareça um inválido no campo do ano, o sistema informará o erro e continuará perguntando até que um valor correto seja preenchido.*/
 
-let name = prompt('Informe seu nome: ');
-alert('So serao permitidos ano de nascimento entre 1922 a 2021');
-let date = prompt('Informe ano de nascimento: ');
-let verify = false
-function dateBirthday(date,name){
-    while(verify){
-        if(date >= parseInt(1922) && date <= parseInt(2021)){ //verifica se a data esta entre os anos 1922 e 2021
-            if(isNaN(date==false)){
-                let calcAge = parseInt(2022)-date
-                console.log(' Nome: '+name+'\n Idade:' +calcAge+' anos de idade');
-                verify = true;
-            }else{
-                verify = false;
-            }
-            
-        }else{
-            
-            throw new Error('Data fora do range permitido') // caso esteja fora do range e gerado um erro exclusivo
-        }
-}   }
+let valueName = prompt('Informe seu nome:');
+let valueData;
 
-try { //tratamento de erro
-    console.log(dateBirthday(date, name))
-  } catch (err) {
-    console.log(err.message)
-    //Data fora do range permitido
+alert('\nSo serao permitidos anos  entre 1922 a 2021');
+console.log('--------------------------------------------\n')
+
+function dateBirthday(name) {
+  while (true) {
+
+    valueData = parseInt(prompt('Informe ano de nascimento: '));
+    try {
+      if (isNaN(valueData)) throw `Not a valid number`;
+      if (valueData < 1922 || valueData > 2021) throw `Date outside the allowed range`;
+      break;
+    }
+    catch (error) {
+      console.error(error);
+    }
   }
+  let calcAge = parseInt(2022) - valueData
+  console.log(' Nome: ' + name + '\n Idade: ' + calcAge + ' anos');
+}
+dateBirthday(valueName)
