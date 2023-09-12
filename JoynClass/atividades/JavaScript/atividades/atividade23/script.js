b0f640c08bb58782d9036616e2453534d8ba8ed1
Samuel Santana <samuelsantana95@outlook.com>
2023-09-12 20:28:31 -0300

    exercise 23 included

---
+++ JoynClass/atividades/JavaScript/atividades/atividade23/script.js
@@ -0,0 +1,13 @@
+let radData = "Variavel a ser mudada";
+
+console.log(radData.toLowerCase());//toma a string e converte todos os caracteres para minusculo
+//variavel a ser mudada
+
+
+console.log(radData.toUpperCase());//toma a string e converte todos os caracteres para maiusculo
+//VARIAVEL A SER MUDADA
+
+
+
+//Este pode ser util, por exemplo, se você quer normalizar 
+//todas as entradas de dados do usuário antes de armazenar em um banco de dados.
