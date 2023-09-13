8eb74efee93aaa700f640ce6df594825ea87e093
Samuel Santana <samuelsantana95@outlook.com>
2023-09-12 22:12:49 -0300

    exercise 24 included

---
+++ JoynClass/atividades/JavaScript/atividades/atividade24/script.js
@@ -0,0 +1,10 @@
+const participantes = {
+    "João": 50,
+    "Pedro": 120,
+    "Luiza": 70,
+    "Marcela": 10,
+    "Yuri": 34
+  };
+
+  console.log(participantes);
+//{ 'João': 50, Pedro: 120, Luiza: 70, Marcela: 10, Yuri: 34 }
